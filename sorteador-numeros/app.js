532b3a1acb862bc753d91a655a89a48b2d7a1646
gustavo_cazumba <gustavo.tec1211@gmail.com>
2026-04-02 18:33:13 -0300

    adicinado lista para armazenar numeros sorteados

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -4,5 +4,20 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
     
-};
+    
+    let listaDeNumeros = [];
+    
+    for(let i = 0;i < quantidade;i++){
+        let numero = obterNumeroAleatorio(de,ate);
+        listaDeNumeros.push(numero);
+    }
+
+}
+
+
+function obterNumeroAleatorio(min,max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
 
