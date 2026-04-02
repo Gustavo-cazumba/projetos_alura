5ea0cb1c5aa39dd76ca5f49d00689b478caa1544
gustavo_cazumba <gustavo.tec1211@gmail.com>
2026-04-02 17:45:55 -0300

    adicionado função pra pegar numeros digitados

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,8 @@
+
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    
+};
+
